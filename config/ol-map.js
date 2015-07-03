41d0660a7843b990fd4c3481ab40922c91ef66ff
Madison Treat <madison.treat@tamu.edu>
2015-07-03 14:36:23 -0500

    Added optional layers and switching to map

--- config/ol-map.js
+++ config/ol-map.js
@@ -1,5 +1,8 @@
-var map;
 var view;
+var satLayer;
+var hybLayer;
+var osmLayer;
+var map;
 
 function getCoords(lat, lon) {
    return ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
@@ -11,18 +14,21 @@ function init() {
       zoom: __ZOOM__
    });
    
+   satLayer = new ol.layer.Tile({
+      source: new ol.source.MapQuest({layer: 'sat'})
+   });
+   hybLayer = new ol.layer.Tile({
+      source: new ol.source.MapQuest({layer: 'hyb'})
+   });
+   osmLayer = new ol.layer.Tile({
+      source: new ol.source.MapQuest({layer: 'osm'}),
+      visible: false
+   });
+   
    map = new ol.Map({
       target: 'map',
-      layers: [
-         new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
-         })//,
-//         new ol.layer.Tile({
-//            source: new ol.source.OSM({
-//               crossOrigin: 'anonymous'
-//            })
-//         })
-      ],
+      layers: [satLayer, hybLayer, osmLayer],
+      allOverlays: true,
       view: view
    });
 }
@@ -37,4 +43,16 @@ function zoomTo(level) {
 
 function rotate(deg, lat, lon) {
    view.rotate(deg*Math.PI/180, getCoords(lat, lon));
+}
+
+function removeSatLayer() {
+   satLayer.set('visible', false);
+   hybLayer.set('visible', false);
+   osmLayer.set('visible', true);
+}
+
+function addSatLayer() {
+   osmLayer.set('visible', false);
+   satLayer.set('visible', true);
+   hybLayer.set('visible', true);
 }
