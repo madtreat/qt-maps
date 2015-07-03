5853ea0f84c2d5b176bdddb363d987aec05ce809
Madison Treat <madison.treat@tamu.edu>
2015-07-03 17:32:00 -0500

    Added support for drawing static icon overlays for cockpit display project

--- config/ol-map.js
+++ config/ol-map.js
@@ -4,6 +4,12 @@ var hybLayer;
 var osmLayer;
 var map;
 
+var acIDs = [];
+var acFeatures = [];
+var acIconLayers = []; // one icon per aircraft
+var acElements = []; // one element per aircraft
+var acPopupOverlays = []; // one popup overlay per aircraft
+
 function getCoords(lat, lon) {
    return ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
 }
@@ -29,7 +35,50 @@ function init() {
       target: 'map',
       layers: [satLayer, hybLayer, osmLayer],
       allOverlays: true,
-      view: view
+      view: view,
+      interactions: [],
+      controls: []
+   });
+   
+   map.on('click', function(evt) {
+      var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature, layer) {
+         return feature;
+      });
+      if (feature) {
+         var id = feature.get('id') - 1; // zero based, but ac 0 is not shown, so -1
+         var geometry = feature.getGeometry();
+         var coord = geometry.getCoordinates();
+         acPopupOverlays[id].setPosition(coord);
+         var element = acElements[id];
+         var content = 'id : '+ feature.get('id') + '\n';
+         content += 'alt: ' + feature.get('alt') + '\n';
+         content += 'rng: ' + feature.get('rng') + '\n';
+         content += 'ber: ' + feature.get('ber');
+         $(element).popover({
+            'placement': 'right',
+            'html': true,
+            'content': content
+         });
+         $(element).popover('show');
+      } else {
+         $(acElements).each(function(index) {
+            $(this).popover('destroy');
+         });
+      }
+   });
+   
+   // change mouse cursor when over marker
+   map.on('pointermove', function(e) {
+      if (e.dragging) {
+         $(acElements).each(function(index) {
+            $(this).popover('destroy');
+         });
+         return;
+      }
+      var pixel = map.getEventPixel(e.originalEvent);
+      var hit = map.hasFeatureAtPixel(pixel);
+      map.getTarget().style.cursor = hit ? 'pointer' : '';
    });
 }
 
@@ -55,4 +104,63 @@ function addSatLayer() {
    osmLayer.set('visible', false);
    satLayer.set('visible', true);
    hybLayer.set('visible', true);
+}
+
+function updateAircraft(id, lat, lon, rng, ber, alt) {
+   var feature = acFeatures[id-1];
+   if (feature) {
+      feature.setGeometry(new ol.geom.Point(getCoords(lat, lon)));
+      feature.set('id', id);
+      feature.set('rng', rng);
+      feature.set('ber', ber);
+      feature.set('alt', alt);
+      
+      acIconLayers[id-1].refresh({force:true});
+   }
+}
+
+function addNewAircraft(id, lat, lon, rng, ber, alt) {
+   if (acIDs.indexOf(id) >= 0) {
+      updateAircraft(id, lat, lon, rng, ber, alt);
+   }
+   else {
+      acIDs.push(id);
+      var iconFeature = new ol.Feature({
+         geometry: new ol.geom.Point(getCoords(lat, lon)),
+         id: id,
+         rng: rng,
+         ber: ber,
+         alt: alt
+      });
+      var iconStyle = new ol.style.Style({
+         image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            anchor: [0.5, 24],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: '../src/images/icons/airplane.png'
+         }))
+      });
+      iconFeature.setStyle(iconStyle);
+      acFeatures.push(iconFeature);
+      
+      var vectorSource = new ol.source.Vector({
+         features: [iconFeature]
+      });
+      var vectorLayer = new ol.layer.Vector({
+         source: vectorSource
+      });
+      map.addLayer(vectorLayer);
+      acIconLayers.push(vectorLayer); // so we can access it later
+      
+      var element = document.getElementById('popup' + id);
+      acElements.push(element);
+      var popup = new ol.Overlay({
+         element: element,
+         positioning: 'bottom-center',
+         stopEvent: false
+      });
+      map.addOverlay(popup);
+      acPopupOverlays.push(popup);
+   }
 }
